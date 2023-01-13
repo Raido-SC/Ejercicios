693232713f2b502007ed704ff43c1c8566e82a10
Raido-SC <ftblrichard@gmail.com>
2023-01-13 15:25:03 -0400

    se agregalos tipos

--- src/com/ejercicio/Tema1.java
+++ src/com/ejercicio/Tema1.java
@@ -1,2 +1,35 @@
-package com.ejercicio;public class Tema1 {
+package com.ejercicio;
+
+public class Tema1 {
+
+    public static void main(String[] args) {
+
+
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4;
+        float number5 = 5.1f;
+        double number6 = 6.20d;
+        char letraA = 'a';
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        String nombre = "Richard";
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+
+        System.out.println(letraA);
+
+        System.out.println("Mi nombre es " + nombre);
+
+
+
+    }
 }
